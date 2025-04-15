0eec83505615b3d428a31e73fe563ec1d008a043
Ryuk0777 <114020461+Ryuk0777@users.noreply.github.com>
2025-04-15 20:49:45 +0530

    Fix cursor

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -3,45 +3,51 @@ import gsap from 'gsap'
 
 const Cursor = ({cursorRef}) => {
 
+    const handleMousemove = (e)=>{
+        e.preventDefault();
+        gsap.to(cursorRef.current, {
+            x: e.clientX - 15,
+            y: e.clientY - 15,
+            duration: 0.5,
+            ease: "power2.Out",
+        })
+    }
+
+    const handleMousedown =  (e)=>{
+        e.preventDefault();
+        gsap.to(cursorRef.current, {
+            scale: 0.8,
+            duration: 0.5,
+            ease: "power2.Out",
+        })
+    }
+
+    const handleMouseup = (e)=>{
+        e.preventDefault();
+        gsap.to(cursorRef.current, {
+            scale: 1,
+            duration: 0.5,
+            ease: "power2.Out",
+        })
+    }
+
     useEffect(()=>{
-        document.addEventListener("mousemove", (e)=>{
-            e.preventDefault();
-            gsap.to(cursorRef.current, {
-                x: e.clientX - 15,
-                y: e.clientY - 15,
-                duration: 0.5,
-                ease: "power2.Out",
-            })
-        })
+        document.addEventListener("mousemove", handleMousemove)
 
-        document.addEventListener("mousedown", (e)=>{
-            e.preventDefault();
-            gsap.to(cursorRef.current, {
-                scale: 0.8,
-                duration: 0.5,
-                ease: "power2.Out",
-            })
-        })
+        document.addEventListener("mousedown",handleMousedown)
 
-        document.addEventListener("mouseup", (e)=>{
-            e.preventDefault();
-            gsap.to(cursorRef.current, {
-                scale: 1,
-                duration: 0.5,
-                ease: "power2.Out",
-            })
-        })
+        document.addEventListener("mouseup", handleMouseup)
 
         return () => {
-            document.removeEventListener("mousemove", ()=>{})
-            document.removeEventListener("mousedown", ()=>{})
-            document.removeEventListener("mouseup", ()=>{})
+            document.removeEventListener("mousemove", handleMousemove)
+            document.removeEventListener("mousedown", handleMousedown)
+            document.removeEventListener("mouseup", handleMouseup)
         }
 
     }, [])
 
   return (
-    <div ref={cursorRef} className='lg:w-8 lg:h-8 lg:bg-black lg:z-30 lg:fixed lg:rounded-full lg:pointer-events-none cursor-none'>
+    <div ref={cursorRef} className='lg:w-6 lg:h-6 lg:bg-black lg:z-30 lg:fixed lg:rounded-full lg:pointer-events-none cursor-none'>
       
     </div>
   )
